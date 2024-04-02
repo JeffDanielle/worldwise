b79166e7bb621fb2894c6b95b23bacd5d7c2d16c
JeffDanielle <bayotjeff@gmail.com>
2024-04-02 13:05:07 +0800

    210. Building the Pages -17min

--- src/components/PageNavigation.jsx
+++ src/components/PageNavigation.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom"
 import styles from './PageNavigation.module.css'
+import Logo from "./Logo"
 function PageNavigation() {
     return (
         <nav className={styles.nav}>
+            <Logo />
             <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
                 <li>
                     <NavLink to="/pricing">Pricing</NavLink>
                 </li>
                 <li>
                     <NavLink to="/product">Product</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
                 </li>
             </ul>
         </nav>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom"
-import PageNavigation from "../components/PageNavigation"
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
+import PageNavigation from "../components/PageNavigation";
 
-function Homepage() {
-    return (
-        <div>
-            <PageNavigation />
-            <h1 className="test">WorldWise</h1>
-            <Link to="/app">Go to app</Link>
-        </div>
-    )
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNavigation />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to='app' className="cta">Start tracking now</Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage
