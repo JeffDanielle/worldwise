54b308de04f4c10e9630c435132ce3a4ce4526cb
JeffDanielle <bayotjeff@gmail.com>
2024-04-03 11:55:58 +0800

    228. Consuming the CitiesContext -4min

--- src/contexts/CititesContext.jsx
+++ src/contexts/CititesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
@@ -31,4 +31,10 @@ const CitiesProvider = ({ children }) => {
     )
 }
 
-export { CitiesProvider, CitiesContext, };
+function useCities() {
+    const context = useContext(CitiesContext)
+    if (context === undefined) throw new Error('useCities must be used within a CitiesProvider')
+    return context;
+}
+
+export { CitiesProvider, useCities };
